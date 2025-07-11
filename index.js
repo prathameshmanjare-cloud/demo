914916961513e2cf7406a61614b0d22b8eea9836
Prathamesh Manjare <prathameshmanjare3@gmail.com>
2025-07-11 09:16:45 +0530

    done 14

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
+import * as random from "random";
+
 
 const path ="./data.json";
-const date=moment().format();
+
+const makeCommit=(n)=>{
+if(n==0)return simpleGit.push();
+const x=random.default.int(0,54);
+const y=random.default.int(0,6);    
+const date=moment().subtract(1,"y").add(1,"d").add(x,"w").add(y,"d").format();
 
 const data={
     date:date,
 };
+console.log(date);
+jsonfile.writeFile(path,data,()=>{
+    simpleGit().add([path]).commit(date,{'- -date':date}),makeCommit.bind(this,--n);
+});
 
-jsonfile.writeFile(path,data);
+};
 
-simpleGit().add([path]).commit(date,{'- -date':date}).push();
+makeCommit(100);
